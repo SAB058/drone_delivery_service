29be156cadb5fd74754dcdc9f0639041d00091b5
Sultan Arif <111052090+SAB058@users.noreply.github.com>
2023-02-18 21:52:19 +0500

    Add files via upload

---
+++ drone_delivery_service/dronedeliveryservice/adminfrontend.java
@@ -0,0 +1,16 @@
+package com.example.dronedeliveryservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dronedeliveryservice.R;
+
+public class adminfrontend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adminfrontend);
+    }
+}
---
+++ drone_delivery_service/dronedeliveryservice/frontend.java
@@ -0,0 +1,19 @@
+package com.example.dronedeliveryservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dronedeliveryservice.R;
+
+public class frontend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frontend);
+
+
+
+    }
+}
